5d61db3c57ead6aa326794b04ca1f4678abdd186
TerraBreus <studentbreus@gmail.com>
2025-11-03 15:04:00 +0100

    test: added colors to header

--- geometry_game/inc/geo.h
+++ geometry_game/inc/geo.h
@@ -6,13 +6,28 @@
 /*   By: zivanov <zivanov@student.codam.nl>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/03 14:05:20 by zivanov           #+#    #+#             */
-/*   Updated: 2025/11/03 14:29:46 by zivanov          ###   ########.fr       */
+/*   Updated: 2025/11/03 14:59:01 by zivanov          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "mlx.h"
+#include "math.h"
 
 #define WINDOWSIZE 1000
+
+#define RED      0xFF0000
+#define GREEN    0x00FF00
+#define BLUE     0x0000FF
+#define YELLOW   0xFFFF00
+#define CYAN     0x00FFFF
+#define MAGENTA  0xFF00FF
+#define WHITE    0xFFFFFF
+#define BLACK    0x000000
+#define ORANGE   0xFFA500
+#define PURPLE   0x800080
+#define PINK     0xFFC0CB
+#define BROWN    0x8B4513
+#define GRAY     0x808080
 
 typedef struct	s_data {
 	void	*img;
@@ -25,4 +40,5 @@ typedef struct	s_data {
 // F U N C T I O N S
 void	draw_hori_lines(t_data *img);
 void	draw_vert_lines(t_data *img);
+void	draw_line(t_data *img, float angle_deg, int length, int start_x, int start_y, int color);
 void	my_mlx_pixel_put(t_data *data, int x, int y, int color);
