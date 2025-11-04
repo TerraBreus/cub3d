03b8ad55ce790e6f71f4d6c219ef37ecabcbca61
TerraMint <studentbreus@gmail.com>
2025-11-04 21:52:22 +0100

    test: renamed file

--- geometry_game/main.c
+++ geometry_game/main.c
@@ -1,12 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   geometry.c                                         :+:      :+:    :+:   */
+/*   main.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: zivanov <zivanov@student.codam.nl>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/03 12:48:58 by zivanov           #+#    #+#             */
-/*   Updated: 2025/11/04 17:35:45 by zivanov          ###   ########.fr       */
+/*   Updated: 2025/11/04 21:50:27 by zivanov          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include "inc/geo.h"
@@ -14,16 +14,13 @@
 int	main(void)
 {
 	t_mlx	mlx;
-	t_data	img;
 
-	mlx.mlx = mlx_init();
-	mlx.mlx_win = mlx_new_window(mlx.mlx, WINDOWSIZE, WINDOWSIZE, "waaaa");
-	mlx.img = &img;
-	img.img = mlx_new_image(mlx.mlx, WINDOWSIZE, WINDOWSIZE);
-	img.addr = mlx_get_data_addr(img.img, &img.bits_per_pixel, &img.line_length, &img.endian);
-	draw_grid(&img, LINES);
-	mlx_put_image_to_window(mlx.mlx, mlx.mlx_win, img.img, 0, 0);
+	init_data(&mlx);
+	draw_grid(&(mlx.grid), LINES);
+	//draw_keyboard(&(mlx.cursor), mlx.keyboard);
+	mlx_put_image_to_window(mlx.mlx, mlx.mlx_win, mlx.grid.img, 0, 0);
 	mlx_hook(mlx.mlx_win, BUTTONPRESS, (1L<<2), square_on_click, &mlx);
+	mlx_hook(mlx.mlx_win, KEYPRESS, (1L<<0), move_keyboard, &mlx);
 	mlx_loop(mlx.mlx);
 	return (0);
 }
