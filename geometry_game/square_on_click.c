1533fea8e7d217579cdf6d1cb3b9478a528b5c83
TerraMint <studentbreus@gmail.com>
2025-11-04 21:55:04 +0100

    test: adding filenames

--- geometry_game/square_on_click.c
+++ geometry_game/square_on_click.c
@@ -6,7 +6,7 @@
 /*   By: zivanov <zivanov@student.codam.nl>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/04 17:33:56 by zivanov           #+#    #+#             */
-/*   Updated: 2025/11/04 17:36:03 by zivanov          ###   ########.fr       */
+/*   Updated: 2025/11/04 21:32:29 by zivanov          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,16 +20,16 @@ int	square_on_click(int keycode, int x, int y, t_mlx *mlx)
 	{
 		if (mlx->map[x][y] == 0)
 		{
-			fill_square(mlx->img, x, y, BLUE);
+			fill_square(&(mlx->grid), x, y, BLUE);
 			mlx->map[x][y] = 1;
 		}
 		else
 		{
-			fill_square(mlx->img, x, y, BLACK);
+			fill_square(&(mlx->grid), x, y, BLACK);
 			mlx->map[x][y] = 0;
 		}
-		draw_grid(mlx->img, LINES);
-		mlx_put_image_to_window(mlx->mlx, mlx->mlx_win, mlx->img->img, 0, 0);
+		draw_grid(&(mlx->grid), LINES);
+		mlx_put_image_to_window(mlx->mlx, mlx->mlx_win, mlx->grid.img, 0, 0);
 	}
 	(void) mlx;
 	return (0);
