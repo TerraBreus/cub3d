4ce8dec5e97f5e70d7efa927b8391f18a715593b
TerraMint <studentbreus@gmail.com>
2025-11-10 20:20:06 +0100

    added new functions

--- geometry_game/main.c
+++ geometry_game/main.c
@@ -3,12 +3,13 @@
 /*                                                        :::      ::::::::   */
 /*   main.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: zivanov <zivanov@student.codam.nl>         +#+  +:+       +#+        */
+/*   By: terrabuntu <terrabuntu@student.42.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/03 12:48:58 by zivanov           #+#    #+#             */
-/*   Updated: 2025/11/04 21:50:27 by zivanov          ###   ########.fr       */
+/*   Updated: 2025/11/06 15:16:46 by zivanov          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
 #include "inc/geo.h"
 
 int	main(void)
@@ -17,10 +18,11 @@ int	main(void)
 
 	init_data(&mlx);
 	draw_grid(&(mlx.grid), LINES);
-	//draw_keyboard(&(mlx.cursor), mlx.keyboard);
-	mlx_put_image_to_window(mlx.mlx, mlx.mlx_win, mlx.grid.img, 0, 0);
+	draw_keyboard(&(mlx.keyboard), mlx.keyboard_pos);
+	merge_images_and_push_to_window(&mlx);
 	mlx_hook(mlx.mlx_win, BUTTONPRESS, (1L<<2), square_on_click, &mlx);
 	mlx_hook(mlx.mlx_win, KEYPRESS, (1L<<0), move_keyboard, &mlx);
+	mlx_hook(mlx.mlx_win, 06, (1L << 6), move_mouse, &mlx);
 	mlx_loop(mlx.mlx);
 	return (0);
 }
