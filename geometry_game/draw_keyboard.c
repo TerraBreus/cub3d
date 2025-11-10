bd1490df43d8fc21cfedd477a7fe90269194fbf8
TerraMint <studentbreus@gmail.com>
2025-11-10 20:18:30 +0100

    test: added draw_keyboard

--- geometry_game/draw_keyboard.c
+++ geometry_game/draw_keyboard.c
@@ -1,6 +1,17 @@
 
 #include "inc/geo.h"
 
-void	draw_keyboard(t_data *img, float pos[2])
+void	draw_keyboard(t_data *img, int pos[2])
 {
+	int			i;
+	static int	old_pos[2];
+
+	for (i = 0; i < 10; i++)
+		draw_line(img, 0, 10, old_pos[0], old_pos[1] + i, BLACK);
+
+	for (i = 0; i < 10; i++)
+		draw_line(img, 0, 10, pos[0], pos[1] + i, RED);
+	old_pos[0] = pos[0];
+	old_pos[1] = pos[1];
+}
 	
