adb5c3cb86ae2dc972b6974dee921b21ebd43b4c
TerraMint <studentbreus@gmail.com>
2025-11-10 20:19:10 +0100

    added move_mouse

---
+++ geometry_game/move_mouse.c
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   move_mouse.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: zivanov <zivanov@student.codam.nl>         +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/11/06 15:13:00 by zivanov           #+#    #+#             */
+/*   Updated: 2025/11/06 17:09:07 by zivanov          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "inc/geo.h"
+
+int	move_mouse(int x, int y, t_mlx *mlx)
+{
+
+	if (mlx->mouse_pos[0] != x || mlx->mouse_pos[1] != y)
+	{
+		mlx->mouse_pos[0] = x;
+		mlx->mouse_pos[1] = y;
+		draw_mouse(&(mlx->mouse), mlx->mouse_pos);
+		merge_images_and_push_to_window(mlx);
+	}
+	return (0);
+}
