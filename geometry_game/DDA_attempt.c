5f0304b75a28cc27f7fb3ea6f7e3a84ac4b5b3ae
TerraMint <studentbreus@gmail.com>
2025-11-17 22:02:46 +0100

    starting new DDA attempt

--- geometry_game/DDA_attempt.c
+++ geometry_game/DDA_attempt.c
@@ -12,6 +12,7 @@
 
 #include "inc/geo.h"
 
+/*
 double	calc_ray_length(float angle, double ds, double pos_x, double pos_y, t_mlx *mlx)
 {
 	//see whether ds_x or ds_y is bigger
@@ -73,88 +74,25 @@ double	calc_ray_length(float angle, double ds, double pos_x, double pos_y, t_mlx
 	}
 	return (ds);
 }
-
-void	cast_ray(t_data *img, float angle, int pix_x, int pix_y, t_mlx *mlx)
+*/
+double	calc_ray_length(double angle_r, double x, double y, t_mlx *mlx)
 {
-	int		cell_size;
-	double	pos_x;
-	double	pos_y;
-	double	ray_length;
+	double	d_xy[2];		//dx and dy respect. Direction of the ray in x & y dir.
+	double	ds_xy[2];		//the length of the ray for going to the next x/y line.
+	double	nxt_xy[2];		//the dist. to the next x or y line (hor/ver).
+	double	result;			//the total length of the ray;
+	
 
-	cell_size = (WINDOWSIZE / LINES);
-	pos_x = ((double) pix_x / cell_size);
-	pos_y = ((double) pix_y / cell_size);
-	printf("%f, %f, %f\n", angle, pos_x, pos_y);
-	ray_length = calc_ray_length(angle, 0, pos_x, pos_y, mlx);
+}
+
+void	cast_ray(t_data *img, double angle, int  pix_x, int pix_y y, t_mlx *mlx)
+{
+	double		ray_length;
+	double		cell_size;
+
+	cell_size = (double) WINDOWSIZE / LINES;
+	printf("%f, %f, %f\n", (angle / 180) * M_PI, (double) (pix_x / cell_size), (double)  (pix_y / cell_size));
+	ray_length = calc_ray_length((angle / 180) * M_PI, (double) (pix_x / cell_size), (double)  (pix_y / cell_size), t_mlx *mlx);
 	draw_line(img, angle, (int) (ray_length * cell_size), pix_x, pix_y, ORANGE);
 }
 
-/*
-void	cast_ray(t_data *img, float angle, int pix_x, int pix_y)
-{
-	double	nxt_vline;
-	double	dist_vline;
-	double	dist_hline;
-	double	nxt_hline;
-	int		cell_size;
-	double	pos_x;
-	int		dir_x;
-	double	pos_y;
-	int		dir_y;
-
-	cell_size = (WINDOWSIZE / LINES);
-	pos_x = ((double) pix_x / cell_size);
-	pos_y = ((double) pix_y / cell_size);
-	nxt_hline = fmod(pos_y, dist_hline);
-	nxt_vline = fmod(pos_x, dist_vline);
-	if (angle > -90 && angle < 90)
-	{
-		nxt_vline = 1 - nxt_vline;
-		dir_x = 0;
-	}
-	else
-	{
-		dir_x = 180;
-		nxt_vline *= -1;
-	}
-	printf("pos_x; %f nxt: %f\n", pos_x, nxt_vline);
-	if (angle > 1 && angle < 180)
-	{
-		nxt_hline = 1 - nxt_hline; //maybe the other way around so we know the direction (l/r) based on the sign and then caal fabs() when u want the length
-		dir_y = 90;
-	}
-	else
-	{
-		dir_y = -90;
-		nxt_hline *= -1;
-	}
-	printf("pos_y; %f nxt: %f\n", pos_y, nxt_hline);
-	draw_line(img, dir_x, (int) (fabs(nxt_vline) * cell_size), pix_x, pix_y, RED);
-	draw_line(img, dir_y, (int) (fabs(nxt_hline) * cell_size), pix_x, pix_y, GREEN);
-
-	double	ds_x;
-	double	ds_y;
-
-	ds_x = fabs(nxt_vline) / cos((angle / 180) * M_PI);
-	ds_y = fabs(nxt_hline) / sin((angle / 180) * M_PI);
-	printf("ds_x %f, ds_y %f, bool %b\n", fabs(ds_x), fabs(ds_y), fabs(ds_x) < fabs(ds_y));
-
-	if (fabs(ds_x) < fabs(ds_y))
-	{
-		printf("\ndsx:%i\n", (int) (ds_x * cell_size));
-		draw_line(img, angle, (int) (fabs(ds_x) * cell_size), pix_x, pix_y, ORANGE); 
-		pos_x += nxt_vline; //(or -=)
-		pos_y += (ds_x * sin(angle));
-	}
-	else
-	{
-		printf("\ndsy:%i\n", (int) (ds_y * cell_size));
-
-		draw_line(img, angle, (int) (fabs(ds_y) * cell_size), pix_x, pix_y, ORANGE); 
-		pos_x += ds_y * cos(angle);
-		pos_y += nxt_hline;
-	}
-	//Then do the whole thing over again.
-	printf("\n x: %f, y: %f\n", pos_x, pos_y);
-}
-*/
