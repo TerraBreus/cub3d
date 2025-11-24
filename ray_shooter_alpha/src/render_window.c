fd88732cceb454c77349b68634a1a62bbcd2fd90
TerraMint <studentbreus@gmail.com>
2025-11-24 16:22:20 +0100

    ray: added function to merge grid and user quickly

---
+++ ray_shooter_alpha/src/render_window.c
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   render_window.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: zivanov <zivanov@student.codam.nl>         +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/11/24 11:17:14 by zivanov           #+#    #+#             */
+/*   Updated: 2025/11/24 11:22:39 by zivanov          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../inc/ray.h"
+
+void	render_window(t_data *data, bool ray)
+{
+	if (ray == true)
+	{
+		printf("ray  npt implemented yet\n");
+	}
+	else
+	{
+		mlx_put_image_to_window(data->mlx, data->mlx_win, data->grid.img, 0, 0);
+		mlx_put_image_to_window(data->mlx, data->mlx_win, data->user.img.img, (int) data->user.pos[0], (int) data->user.pos[1]);
+	}
+}
+	
