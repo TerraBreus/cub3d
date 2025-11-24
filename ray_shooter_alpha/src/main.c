d2a3f82db1026e2ba6ea5042bb1f5d12ad916371
TerraMint <studentbreus@gmail.com>
2025-11-24 16:22:59 +0100

    ray: added event handlers to main

--- ray_shooter_alpha/src/main.c
+++ ray_shooter_alpha/src/main.c
@@ -6,7 +6,7 @@
 /*   By: zivanov <zivanov@student.codam.nl>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/22 08:58:47 by zivanov           #+#    #+#             */
-/*   Updated: 2025/11/22 11:50:29 by zivanov          ###   ########.fr       */
+/*   Updated: 2025/11/24 16:17:35 by zivanov          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,8 @@ int	main(void)
 	t_data	data;
 
 	init_data(&data);
-	//hook_events();
+	mlx_hook(data.mlx_win, KEYPRESS, (1L<<0), keyboard, &data);
+	mlx_hook(data.mlx_win, BUTTONPRESS, (1L<<2),mouse_click , &data);
 	mlx_loop(data.mlx);
 	return (0);
 }
