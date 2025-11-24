c2bf5ebfcf2357bfc73beaf7c001667c92da1f4e
TerraMint <studentbreus@gmail.com>
2025-11-24 18:47:34 +0100

    ray: create_image for ray img

--- ray_shooter_alpha/src/init_data.c
+++ ray_shooter_alpha/src/init_data.c
@@ -6,7 +6,7 @@
 /*   By: zivanov <zivanov@student.codam.nl>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/22 11:43:14 by zivanov           #+#    #+#             */
-/*   Updated: 2025/11/24 11:23:40 by zivanov          ###   ########.fr       */
+/*   Updated: 2025/11/24 18:29:29 by zivanov          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,6 +17,7 @@ void	init_data(t_data *data)
 	data->mlx = mlx_init();
 	data->mlx_win = mlx_new_window(data->mlx, WINDOWSIZE, WINDOWSIZE, "let him cook");
 	my_mlx_create_image(data->mlx, &(data->grid), WINDOWSIZE);
+	my_mlx_create_image(data->mlx, &(data->rays), WINDOWSIZE);
 	init_grid(data);
 	init_user(data->mlx, &(data->user));
 	render_window(data, false);
