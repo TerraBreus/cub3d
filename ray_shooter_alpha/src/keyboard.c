19b01999d9dc65c132911aeaa85e181a3508fce1
TerraMint <studentbreus@gmail.com>
2025-11-24 17:47:36 +0100

    ray: refactored moveplayer to separate function

--- ray_shooter_alpha/src/keyboard.c
+++ ray_shooter_alpha/src/keyboard.c
@@ -6,24 +6,19 @@
 /*   By: zivanov <zivanov@student.codam.nl>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/24 16:17:53 by zivanov           #+#    #+#             */
-/*   Updated: 2025/11/24 16:18:22 by zivanov          ###   ########.fr       */
+/*   Updated: 2025/11/24 17:46:19 by zivanov          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
+//portable PC/laptop with docking
 
 #include "../inc/ray.h"
 
 int keyboard(int keycode, t_data *data)
 {
-	if (keycode == UP)
-		data->user.pos[1] -= MOVESPEED;
-	else if (keycode == DOWN)
-		data->user.pos[1] += MOVESPEED;
-	else if (keycode == LEFT)
-		data->user.pos[0] -= MOVESPEED;
-	else if (keycode == RIGHT)
-		data->user.pos[0] += MOVESPEED;
+	if (keycode == UP || keycode == DOWN || keycode == LEFT || keycode == RIGHT)
+		move_player(data->user.pos, keycode, data);
 	else
 		printf("key:%i\n", keycode);
-	render_window(data, false);
 	return (0);
 }
