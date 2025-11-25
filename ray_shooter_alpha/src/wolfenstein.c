c7926221ce03e0acd47396b190cb49621c29d295
TerraMint <studentbreus@gmail.com>
2025-11-25 11:30:30 +0100

    ray: added wolfenstein engine!!!

---
+++ ray_shooter_alpha/src/wolfenstein.c
@@ -0,0 +1,48 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   wolfenstein.c                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: zivanov <zivanov@student.codam.nl>         +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/11/25 10:27:33 by zivanov           #+#    #+#             */
+/*   Updated: 2025/11/25 11:20:48 by zivanov          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../inc/ray.h"
+
+static int convert_to_vert_line(double ray_length)
+{
+	int	some_fact;
+	double	cell_size;
+
+	cell_size = (double) WINDOWSIZE / LINES;
+	some_fact = (int) 10 * (cell_size / ray_length);
+
+	if (some_fact > (WINDOWSIZE / 2))
+			some_fact = WINDOWSIZE / 2;
+	return (some_fact);
+}
+
+void	wolfenstein(t_img *img, double angle, int  pix_x, int pix_y, t_data *mlx)
+{
+	double		dr;
+	double		ray_length;
+	double		cell_size;
+	int			screen_length;
+	int			i;
+
+	fill_img_with(BLACK, img);
+	dr = (double) (FOV) / WINDOWSIZE;
+	cell_size = (double) WINDOWSIZE / LINES;
+	for (i = -(WINDOWSIZE / 2); i < (WINDOWSIZE / 2) - 1; i++)
+	{
+		ray_length = calc_ray_length(((angle + (i * dr)) / 180) * -M_PI, ((double) pix_x) / cell_size, ((double)  pix_y) / cell_size, mlx);
+		screen_length = convert_to_vert_line(ray_length);
+		//draw_line(img, angle + (i * dr), (int) (ray_length * cell_size), pix_x, pix_y, ORANGE);
+		draw_line(img, 90, screen_length / 2, i + (WINDOWSIZE / 2), (WINDOWSIZE / 2), WHITE);
+		draw_line(img, -90, screen_length / 2, i + (WINDOWSIZE / 2), (WINDOWSIZE / 2), WHITE);
+	}
+	render_window(mlx, true);
+}
