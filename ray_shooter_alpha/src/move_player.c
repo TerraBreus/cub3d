20d76831f996769ddd15133707486d3a52224aa0
TerraMint <studentbreus@gmail.com>
2025-11-27 17:38:23 +0100

    ray: added new movement with direction

--- ray_shooter_alpha/src/move_player.c
+++ ray_shooter_alpha/src/move_player.c
@@ -6,7 +6,7 @@
 /*   By: zivanov <zivanov@student.codam.nl>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/24 17:43:29 by zivanov           #+#    #+#             */
-/*   Updated: 2025/11/25 09:42:41 by zivanov          ###   ########.fr       */
+/*   Updated: 2025/11/27 17:32:35 by zivanov          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,38 +14,54 @@
 
 void	move_player(float pos[2], int keycode, t_data *data)
 {
-	if (keycode == UP)
-	{
-		if (wall_collision((int) pos[0], (int) pos[1] - MOVESPEED, data))
-			return ;
-		if (wall_collision((int) pos[0] + USERSIZE, (int) pos[1] - MOVESPEED, data))
-			return ;
-		pos[1] -= MOVESPEED;
-	}
-	else if (keycode == DOWN)
-	{
+	int	dx;
+	int	dy;
 
-		if (wall_collision(pos[0], pos[1] + MOVESPEED + USERSIZE, data))
-			return ;
-		if (wall_collision(pos[0] + USERSIZE, pos[1] + MOVESPEED + USERSIZE, data))
-			return ;
-		pos[1] += MOVESPEED;
-	}
-	else if (keycode == LEFT)
+
+	if (keycode == DOWN)
 	{
-		if (wall_collision(pos[0] - MOVESPEED, pos[1] + USERSIZE, data))
+		dx = cos((data->user.direction) * (M_PI / 180)) * MOVESPEED;
+		dy = -sin((data->user.direction) * (M_PI / 180)) * MOVESPEED;
+		if (wall_collision((int) pos[0] - dx, (int) pos[1] + dy - MOVESPEED, data))
 			return ;
-		if (wall_collision(pos[0] - MOVESPEED, pos[1], data))
+		if (wall_collision((int) pos[0] + USERSIZE - dx, (int) pos[1] + dy - MOVESPEED, data))
 			return ;
-		pos[0] -= MOVESPEED;
+		pos[1] += dy;
+		pos[0] -= dx;
+	}
+	else if (keycode == UP)
+	{
+		dx = cos((data->user.direction) * (M_PI / 180)) * MOVESPEED;
+		dy = -sin((data->user.direction) * (M_PI / 180)) * MOVESPEED;
+		if (wall_collision(pos[0] + dx, pos[1] - dy + MOVESPEED + USERSIZE, data))
+			return ;
+		if (wall_collision(pos[0] + dx + USERSIZE, pos[1] - dy + MOVESPEED + USERSIZE, data))
+			return ;
+		pos[1] -= dy;
+		pos[0] += dx;
+
 	}
 	else if (keycode == RIGHT)
 	{
-		if (wall_collision(pos[0] + MOVESPEED + USERSIZE, pos[1], data))
+		dx = cos((data->user.direction + 90) * (M_PI / 180)) * MOVESPEED;
+		dy = -sin((data->user.direction + 90) * (M_PI / 180)) * MOVESPEED;
+		if (wall_collision(pos[0] + dx - MOVESPEED, pos[1] - dy + USERSIZE, data))
 			return ;
-		if (wall_collision(pos[0] + MOVESPEED + USERSIZE, pos[1] + USERSIZE, data))
+		if (wall_collision(pos[0] - MOVESPEED + dx, pos[1] - dy, data))
 			return ;
-		pos[0] += MOVESPEED;
+		pos[1] -= dy;
+		pos[0] += dx;
+	}
+	else if (keycode == LEFT)
+	{
+		dx = cos((data->user.direction + 90) * (M_PI / 180)) * MOVESPEED;
+		dy = -sin((data->user.direction + 90) * (M_PI / 180)) * MOVESPEED;
+		if (wall_collision(pos[0] + MOVESPEED + USERSIZE - dx, pos[1] + dy, data))
+			return ;
+		if (wall_collision(pos[0] + MOVESPEED + USERSIZE - dx, pos[1] + USERSIZE + dy, data))
+			return ;
+		pos[1] += dy;
+		pos[0] -= dx;
 	}
 	render_window(data, false);
 }
