c71684afc5125fbc5f2a8e822025f840d5e80820
TerraMint <studentbreus@gmail.com>
2025-12-04 11:16:58 +0100

    ray: added in comment where new wolfenstein will be called

--- ray_shooter_alpha/src/render_window.c
+++ ray_shooter_alpha/src/render_window.c
@@ -6,7 +6,7 @@
 /*   By: zivanov <zivanov@student.codam.nl>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/24 11:17:14 by zivanov           #+#    #+#             */
-/*   Updated: 2025/11/27 11:07:56 by zivanov          ###   ########.fr       */
+/*   Updated: 2025/12/04 09:26:13 by zivanov          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,6 +27,7 @@ void	render_window(t_data *data, bool change_ray)
 	if (ray == true)
 	{
 		wolfenstein(&data->rays, data->user.direction, data->user.pos[0], data->user.pos[1],  data);
+		//wolfenstein_tex();
 		mlx_put_image_to_window(data->mlx, data->mlx_win, data->rays.img, 0, 0);
 	}
 	else
