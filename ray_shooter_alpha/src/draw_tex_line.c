8aa2762ed2c3dfa63cb70fe67007e14a25ece075
TerraMint <studentbreus@gmail.com>
2025-12-07 17:21:40 +0100

    ray: added prototype for drawingtextures

---
+++ ray_shooter_alpha/src/draw_tex_line.c
@@ -0,0 +1,39 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   draw_tex_line.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: zivanov <zivanov@student.codam.nl>         +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/12/07 16:42:24 by zivanov           #+#    #+#             */
+/*   Updated: 2025/12/07 17:17:10 by zivanov          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../inc/ray.h"
+
+unsigned int	get_pixel_color(t_img *data, int x, int y)
+{
+	char *dst;
+	
+	dst = data->addr + (y * data->line_length + x * (data->bits_per_pixel / 8));
+	return (*(unsigned int*) dst);
+}
+
+void	draw_tex_line(t_img *img, t_tex *tex, t_ray *ray, int i)
+{
+	int		which_line;
+	int		step_size;
+	int		j;
+	unsigned int	temp;
+	
+	which_line = ray->pos_wall_hit * tex->width;
+	step_size = tex->height / ray->pixel_length;
+
+	for (j = 0; j < (ray->pixel_length / 2); j++)
+	{
+		temp = get_pixel_color(&tex->img, which_line, (j + (ray->pixel_length / 2)) * step_size);
+		my_mlx_pixel_put(img, i, (WINDOWSIZE / 2) + j, temp);
+	}
+}
+
