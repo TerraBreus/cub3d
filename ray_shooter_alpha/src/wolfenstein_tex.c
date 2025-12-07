26b845e6506d46c6430f2851b057d577fdaf10fc
TerraMint <studentbreus@gmail.com>
2025-12-07 19:50:31 +0100

    ray: fixed direction for wolfenstein_tex engine

--- ray_shooter_alpha/src/wolfenstein_tex.c
+++ ray_shooter_alpha/src/wolfenstein_tex.c
@@ -22,6 +22,7 @@ void	wolfenstein_tex(t_tex *tex, t_img *img, t_data *data, t_user *user)
 	//double	user_dir;
 	double		user_x;
 	double		user_y;
+	fill_img_with(BLACK, img);
 
 
 	user_x = user->pos[0] * ((double) LINES / WINDOWSIZE);
@@ -30,10 +31,8 @@ void	wolfenstein_tex(t_tex *tex, t_img *img, t_data *data, t_user *user)
 	for (i = 0; i < WINDOWSIZE; i++)
 	{ 
 		//-45 to 45 degrees (though in degree) with respect to the users original direction
-		angle = (((i - ((double) WINDOWSIZE / 2)) * dr) + user->direction) * (M_PI / 180);
+		angle = (((i - ((double) WINDOWSIZE / 2)) * dr) + user->direction) * (-M_PI / 180);
 		calc_ray_with_tex(&ray, angle, user_x, user_y, data);
 		draw_tex_line(img, tex, &ray, i);
 	}
-	mlx_put_image_to_window(data->mlx, data->mlx_win, img->img, 0, 0);
-	printf("wolfenstein finished \n");
 }
