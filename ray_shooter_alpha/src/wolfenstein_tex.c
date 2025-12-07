5a59abebe5e914cbc06b6c00bb1f3dd962abbe95
TerraMint <studentbreus@gmail.com>
2025-12-07 17:22:19 +0100

    ray: added very buggy textures to the wolfenstein engine

--- ray_shooter_alpha/src/wolfenstein_tex.c
+++ ray_shooter_alpha/src/wolfenstein_tex.c
@@ -6,7 +6,7 @@
 /*   By: zivanov <zivanov@student.codam.nl>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/12/04 09:19:49 by zivanov           #+#    #+#             */
-/*   Updated: 2025/12/04 09:52:42 by zivanov          ###   ########.fr       */
+/*   Updated: 2025/12/07 17:18:18 by zivanov          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,17 +19,21 @@ void	wolfenstein_tex(t_tex *tex, t_img *img, t_data *data, t_user *user)
 	double	angle;
 	double	dr;
 
-	double	user_dir;
-	int		user_x;
-	int		user_y;
+	//double	user_dir;
+	double		user_x;
+	double		user_y;
 
+
+	user_x = user->pos[0] * ((double) LINES / WINDOWSIZE);
+	user_y = user->pos[1] * ((double) LINES / WINDOWSIZE);
 	dr = (double) FOV / WINDOWSIZE;
 	for (i = 0; i < WINDOWSIZE; i++)
 	{ 
 		//-45 to 45 degrees (though in degree) with respect to the users original direction
 		angle = (((i - ((double) WINDOWSIZE / 2)) * dr) + user->direction) * (M_PI / 180);
-		//calc_ray_with_tex(&ray, angle, user_x, user_y, data);			TODO
-		//draw_tex_line(&ray, img);										TODO
-
+		calc_ray_with_tex(&ray, angle, user_x, user_y, data);
+		draw_tex_line(img, tex, &ray, i);
 	}
+	mlx_put_image_to_window(data->mlx, data->mlx_win, img->img, 0, 0);
+	printf("wolfenstein finished \n");
 }
