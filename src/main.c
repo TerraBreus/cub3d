79e34ab2b5f3f4e9699a132d1f0b49b6d9df2135
TerraBreus <studentbreus@gmail.com>
2025-12-28 11:28:06 +0100

    basic idea for main loop without parsing

--- src/main.c
+++ src/main.c
@@ -6,19 +6,20 @@
 /*   By: zivanov <zivanov@student.codam.nl>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/12/27 13:57:38 by zivanov           #+#    #+#             */
-/*   Updated: 2025/12/27 14:03:34 by zivanov          ###   ########.fr       */
+/*   Updated: 2025/12/28 11:23:55 by zivanov          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <stdio.h>
-#include "..//mlx/mlx.h"
+#include "../inc/cub3d.h"
 
 int	main(void)
 {
-	void *ptr;
-	printf("oi ya fekker\n");
-	ptr = mlx_init();
-	if (ptr == NULL)
-		printf("ppops\n");
+	t_data	data;
+
+	if (init_data(&data) != 0)
+		return (-1);
+	//hook_events();
+	mlx_loop(data.mlx);
+	//clean_up();
 	return (0);
 }
