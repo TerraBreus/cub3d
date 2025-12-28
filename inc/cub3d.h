b71c005495eb9e4b44aad3a24e673902fe2fb3df
TerraBreus <studentbreus@gmail.com>
2025-12-28 11:58:06 +0100

    added info comments to structures and changed windowsize

--- inc/cub3d.h
+++ inc/cub3d.h
@@ -6,7 +6,7 @@
 /*   By: zivanov <zivanov@student.codam.nl>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/12/28 11:07:29 by zivanov           #+#    #+#             */
-/*   Updated: 2025/12/28 11:46:44 by zivanov          ###   ########.fr       */
+/*   Updated: 2025/12/28 11:57:40 by zivanov          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,8 +15,16 @@
 #include <stdbool.h>
 #include <stdlib.h>
 
-#define WINDOWHEIGHT 720
-#define WINDOWLENGTH 1280
+#define WINDOWHEIGHT 1080
+#define WINDOWLENGTH 1920
+
+/*
+ * Simple structure for mlx data.
+ * The win_h and win_l parameters
+ * are for future resizing of windows.
+ * This is not a subject requirement,
+ * but perhaps useful just in case.
+*/
 
 typedef struct s_mlx_data
 {
@@ -25,5 +33,19 @@ typedef struct s_mlx_data
 	int		win_h;
 	int		win_l;
 }	t_mlx_data;
+
+/*
+ * All the necessary information from parsing.
+ * Should contain:
+ * 		- The map.
+ * 		- Texture pathnames.
+ * 		- Starting position player
+ * 		- Colors for Floor & ceiling
+*/
+
+typedef struct s_input
+{
+	char	**map;
+}	t_input;
 
 int		init_data(t_mlx_data *data);
