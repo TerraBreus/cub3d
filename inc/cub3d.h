0c302131a506e05f207694bdf5e292b542b7a8c1
TerraBreus <studentbreus@gmail.com>
2025-12-28 11:47:05 +0100

    refactor for norminette

--- inc/cub3d.h
+++ inc/cub3d.h
@@ -6,7 +6,7 @@
 /*   By: zivanov <zivanov@student.codam.nl>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/12/28 11:07:29 by zivanov           #+#    #+#             */
-/*   Updated: 2025/12/28 11:21:50 by zivanov          ###   ########.fr       */
+/*   Updated: 2025/12/28 11:46:44 by zivanov          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,9 +18,12 @@
 #define WINDOWHEIGHT 720
 #define WINDOWLENGTH 1280
 
-typedef struct s_data {
+typedef struct s_mlx_data
+{
 	void	*mlx;
 	void	*mlx_win;
-}	t_data;
+	int		win_h;
+	int		win_l;
+}	t_mlx_data;
 
-int		init_data(t_data *data);
+int		init_data(t_mlx_data *data);
