3027144faef70b8241181f12cb588765f4486a9e
TerraBreus <studentbreus@gmail.com>
2025-12-30 17:16:23 +0100

    split enums and structures into separate headers for readability

---
+++ inc/enums.h
@@ -0,0 +1,77 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   enums.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: zivanov <zivanov@student.codam.nl>         +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/12/30 17:07:04 by zivanov           #+#    #+#             */
+/*   Updated: 2025/12/30 17:15:57 by zivanov          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+// --- --- --- E N U M S --- --- --- ---
+// - - - - - - - - - - - - - - - - - - -
+// (the bridge between readable
+// 		and executable code)
+
+/*
+ * X11 Masking events. (very difficult and somewhat beyond the scope)
+ * These enums are used with 
+ * bit operators to specify what device we are getting input from.
+ * In essence, this can be the keyboard or the mouse.
+ * 
+ * There is an important destinction between KEY_PRESS and KEY_RELEASE:
+ * When holding down a key, we will first send a single
+ * key press and after half a second start spamming this key.
+ * (you can easily see this in any text editor when holding down a key)
+ *
+ * This makes movement (in f.e. a videogame) stuttered:
+ * you move a little, then stand still and start moving again.
+ *
+ * Thus we need to combine both KEYPRESS and a KEYRELEASE event;
+ * one to start the movement and one to stop it.
+*/
+
+typedef enum e_mask
+{
+	NO_EVENT_MASK = 0L,
+	KEY_PRESS_MASK = (1L << 0),
+	KEY_RELEASE_MASK = (1L << 1),
+	BUTTON_PRESS_MASK = (1L << 2),
+	STRUCTURE_NOTIFY_MASK = (1L << 17)
+}	t_mask;
+
+/*
+ * X11 Events with their corresponding values.
+ * In addition to the masking, these enums are used to 
+ * identify what kind of event is triggered in 
+ * the mlx_hook() function.
+ * (see hook_events.c)
+ * for more documentation see:
+ * https://harm-smits.github.io/42docs/libs/minilibx/events.html
+*/
+
+typedef enum e_events
+{
+	ON_KEYDOWN = 2,
+	ON_KEYUP = 3,
+	ON_MOUSEMOVE = 6,
+	ON_DESTROY = 17
+}	t_events;
+
+/*
+ * To make the texture structure more readable
+ * we use a simple enum to keep track of
+ * which side of the cube we are talking about.
+ * Usage would be:
+ * cub3d->textures.paths[NO];
+*/
+
+typedef enum e_dir
+{
+	NO = 0,
+	EA = 1,
+	SO = 2,
+	WE = 3
+}	t_dir;
---
+++ inc/structs.h
@@ -0,0 +1,115 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   structs.h                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: zivanov <zivanov@student.codam.nl>         +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/12/30 17:05:59 by zivanov           #+#    #+#             */
+/*   Updated: 2025/12/30 17:15:53 by zivanov          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+// --- --- --- S T R U C T U R E S --- --- ---
+// - - - - - - - - - - - - - - - - - - - - - -
+// (because we can't pass a hundred
+// 		parameters to a function)
+
+/*	-- MLX STRUCTURE --
+ * Simple structure holding relevant data
+ * for the graphical library mlx.
+ * 
+ * Main usage of the library is
+ * the creation and maintenance 
+ * of windows, events and images
+
+ * More info on this library can be found
+ * in the mlx/mlx.h header.
+ *
+ * The win_h and win_l parameters
+ * are for future resizing of windows.
+ * This is not a subject requirement,
+ * but perhaps useful just in case.
+*/
+
+typedef struct s_mlx
+{
+	void	*instance;
+	void	*window;
+	int		win_h;
+	int		win_l;
+}	t_mlx;
+
+/*	-- LEVEL STRUCTURE --
+ * Metadata of the level given from the parser.
+ * Should contain:
+ * 		- The map.
+ * 		- Starting position player
+ * 		- Colors for Floor & ceiling
+*/
+
+typedef struct s_level
+{
+	char	**map;
+	int		x_row;
+	int		y_col;
+	int		player_pos_x;
+	int		player_pos_y;
+	int		player_dir_deg;
+}	t_level;
+
+/*
+ * -- TEXTURE STRUCTURE --
+ *  Paths will be given by parsing.
+ *  Execution will create the mlx image structures.
+ *  This can be considered part of the mlx structure,
+ *  yet this was purposely avoided to make a
+ *  distinction between the mlx instance and the mlx images.
+ *
+ *  This structure makes use of the t_dir enums and can be used
+ *  as following: textures.paths[NO/EA/SO/WE] and
+ *  similarly textures.mlx_img[NO/EA/SO/WE]
+*/
+
+typedef struct s_textures
+{
+	char	*paths[4];
+	void	*mlx_img[4];
+}	t_textures;
+
+/*
+ * -- COLOR STRUCTURE --
+ *  In essence this is a simple array of three integers
+ *  yet to avoid the confusion of pointers and arrays
+ *  a decision was made to use a separate structure.
+ *
+ *  This makes parsing colors as a parameter easier and more readable.
+ *  Instead of `some_func(int colors[3]);`
+ *  (above can be confusing when using pointers and arrays combined)
+ *  we can use `some_func(t_colors *colors);`
+ *  (this is a pointer and not an array)
+*/
+
+typedef struct s_color
+{
+	int	r;
+	int	g;
+	int	b;
+}	t_colors;
+
+/*
+ * -- CUB3D STRUCTURE --
+ *  A big meta structure that holds all the
+ *  data needed for the cub3d program to run.
+ *  Most structures have their descriptions given
+ *  in the comments above.
+*/
+
+typedef struct s_cub3d
+{
+	t_textures	texture;
+	t_colors	floor;
+	t_colors	ceiling;
+	t_mlx		mlx_data;
+	t_level		level;
+}	t_cub3d;
